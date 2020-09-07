8079f6fd6a3af1d450d206401155777038081e1a
bridget462 <bridges.dev.create@gmail.com>
2020-09-07 23:18:28 +0900

    particles added

--- src/App.js
+++ src/App.js
@@ -3,13 +3,37 @@ import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import Particles from "react-particles-js";
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 300,
+    },
+    size: {
+      value: 3,
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse",
+      },
+    },
+  },
+};
 
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
+
       {/* 
         
         <FaceRecognition />} */}
