3c960a659b29eaef94a10488d226b4d68b395d61
bridget462 <bridges.dev.create@gmail.com>
2020-09-07 21:42:40 +0900

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation />
+      {/* {<Logo />
+        <ImageLinkFrom />
+        <FaceRecognition />} */}
+    </div>
+  );
 }
 
 export default App;
