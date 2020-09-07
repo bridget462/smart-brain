2a34d5a98bcc9cd9f772d58796b29d7351e8cc74
bridget462 <bridges.dev.create@gmail.com>
2020-09-07 22:44:52 +0900

    button style updated

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,11 +8,9 @@ const ImageLinkForm = () => {
         {"This Magic Brain will detect faces in your pictures. Give it a try!"}
       </p>
       <div className="center">
-        <div className="center form pa4 br3 shadow-5">
+        <div className="center form pa4 br3 shadow-5 ">
           <input className="f4 pa2 w-70 center" type="text" />
-          <button className="f4 w-30 grow link ph3 dib white bg-light-purple">
-            Detect
-          </button>
+          <button className="w-30 f4 dim white bg-light-purple ">Detect</button>
         </div>
       </div>
     </div>
