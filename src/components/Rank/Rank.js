c6a22d1667def896bd37a21181cb6053da799a5a
bridget462 <bridges.dev.create@gmail.com>
2020-09-11 13:55:30 +0900

    user rank is aquired from server and displied on front-end

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,7 @@ import React from "react";
 const Rank = ({ name, entries }) => {
   return (
     <div>
-      <div className="white f3 ma3">{"User, your current rank is ..."}</div>
+      <div className="white f3 ma3">{`${name}, your current rank is ...`}</div>
       <div className="white f1 fw9">{entries}</div>
     </div>
   );
